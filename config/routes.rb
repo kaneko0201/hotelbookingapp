c2d51306d6d5235d225058686b6fd26f6477acdc
kaneko <wakatsuki5656@gmail.com>
2024-09-02 16:40:00 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/account', to: 'users#account'
-  
+  get 'users/profile', to: 'users#profile'
+  root 'users#top'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
