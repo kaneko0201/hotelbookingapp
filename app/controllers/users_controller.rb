11a9dcebd96632bd45205675ad14d50a316e7ade
kaneko <wakatsuki5656@gmail.com>
2024-09-02 16:21:36 +0900

    ページ追加のためメソッドを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
+  def top
+  end
 
   def account
     @user = current_user
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:user_image, :user_name, :user_introduction ))
+      flash[:notice] = "プロフィール情報が変更されました。"
+    else
+      render "edit"
+    end
   end
 end
