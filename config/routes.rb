9c687fbc5d723c18b503108b36d17f351b019571
kaneko <wakatsuki5656@gmail.com>
2024-09-03 16:08:22 +0900

    エラーへの対処

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'users/account', to: 'users#account'
   get 'users/profile', to: 'users#profile'
   root 'users#top'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
