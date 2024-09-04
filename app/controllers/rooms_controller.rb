bd5cf99965ada31c4aba8a68a6d3129d83783943
kaneko <wakatsuki5656@gmail.com>
2024-09-04 17:20:10 +0900

    roomコントローラー実装

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,18 @@
+class RoomsController < ApplicationController
+  def new
+    @room = Room.new
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(params.require(:room).permit(:room_name, :room_introduction, :room_fee, :room_address))
+    if @room.save
+      redirect_to :root
+    else
+      render "new"
+    end
+  end
+end
